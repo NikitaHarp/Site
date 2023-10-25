e85302f237d4bf116a72212afd5ffcee908bc21d
NikitaHarp <robotnikita12@gmail.com>
2023-10-25 20:10:44 +0300

    .

--- js/JavaScript_site_tools.js
+++ js/JavaScript_site_tools.js
@@ -6,6 +6,28 @@
 $(document).ready(function(){
     $('.header-nav__burger').click(function(event){
         $('.header-nav__burger,.navbar-list').toggleClass('active');
-        $('body').toggleClass('lock')
+        $('body').toggleClass('lock');
     });
+    $('nav.navbar-list a').click(function(event){
+        $('.header-nav__burger,.navbar-list').removeClass('active');
+    })
 });
+
+
+$('nav.navbar-list a').on('click', function(e){
+    var anchor = $(this).data('anchor');
+    $('html,body').stop().animate({ scrollTop: $(anchor).offset().top - document.querySelector('.header-box').offsetHeight}, 1000);
+    e.preventDefault();
+  });
+
+  $('div.footer-content__navbar a').on('click', function(e){
+    var anchor = $(this).data('anchor');
+    $('html,body').stop().animate({ scrollTop: $(anchor).offset().top - document.querySelector('.header-box').offsetHeight}, 1000);
+    e.preventDefault();
+  });
+
+  $('div.content__block-faq a').on('click', function(e){
+    var anchor = $(this).data('anchor');
+    $('html,body').stop().animate({ scrollTop: $(anchor).offset().top - document.querySelector('.header-box').offsetHeight}, 1000);
+    e.preventDefault();
+  });
